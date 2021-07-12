0b3a845780758b8d618a47b121da1585fdaf1dcc
Ramil Gadzhiev <rami.gaggi@gmail.com>
2021-07-12 10:51:00 +0300

    Add whitenose

--- task_manager_site/wsgi.py
+++ task_manager_site/wsgi.py
@@ -10,7 +10,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from task_manager_site.settings import BASE_DIR
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager_site.settings')
 
 application = get_wsgi_application()
+
+application = WhiteNoise(
+    application,
+    root=os.path.join(BASE_DIR, 'staticfiles'),
+)
