6d2d8ecbb7763acd09f3d96519a0bbe5a1773e65
Ramil Gadzhiev <rami.gaggi@gmail.com>
2021-07-12 11:02:39 +0300

    Fix whitnoise

--- task_manager_site/wsgi.py
+++ task_manager_site/wsgi.py
@@ -10,14 +10,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from task_manager_site.settings import BASE_DIR
+from task_manager_site.settings import BASE_DIR, DEBUG
 from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager_site.settings')
 
 application = get_wsgi_application()
 
-application = WhiteNoise(
-    application,
-    root=os.path.join(BASE_DIR, 'staticfiles'),
-)
+if not DEBUG:
+    application = WhiteNoise(
+        application,
+        root=os.path.join(BASE_DIR, 'staticfiles'),
+    )
