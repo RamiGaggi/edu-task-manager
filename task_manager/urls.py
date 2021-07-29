1143584624f6f4e3e3a69517bd018b1e432c9f3b
Ramil Gadzhiev <rami.gaggi@gmail.com>
2021-07-29 16:51:24 +0300

    Add url for translation

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tasks.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
